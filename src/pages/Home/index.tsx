074e329849b8a6722c556abad1a99b1d4c0a0af7
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-07-28 11:43:27 -0300

    integrating form with typescript

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,15 +21,23 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
 })
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
   })
 
   const task = watch('task')
   const isSubmitDisabled = !task
 
-  function handleCreateNewCycle() {}
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+  }
 
   return (
     <HomeContainer>
